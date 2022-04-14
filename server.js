1f4abafe946a39348d4d20ae835e36c200749145
tech-codec <varuslimogne@gmail.com>
2022-04-15 01:26:16 +0200

    mise en place du cors pour envoyer des requetes a notre API

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const postRoutes = require('./routes/post.routes');
 require('dotenv').config({path:'./config/.env'});
 require('./config/db');
 const {checkUser,requireAuth} = require('./milddleware/auth.middelware');
+const cors = require('cors');
+
 const app = express();
 
-
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+  }
+  
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cookieParser());
